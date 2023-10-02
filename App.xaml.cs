3a2db8cedf5415ae5be0554b856613445fd09d72
Jake Hathaway <Jake.D.Hathaway@gmail.com>
2023-10-02 16:09:03 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        var height = 600;
+        var width = 400;
+
+        window.Width = width;
+        window.Height = height;
+
+        window.MinimumHeight = height;
+        window.MinimumWidth = width;
+
+        window.MaximumHeight = height;
+        window.MaximumWidth = width;
+
+        return window;
+    }
+}
